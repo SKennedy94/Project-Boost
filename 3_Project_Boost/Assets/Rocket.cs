cfb33dc40bdfba898c6cfda995b9d09a0ce2b69a
Seamus Kennedy <31747877+SKennedy94@users.noreply.github.com>
2018-07-02 09:35:09 -0700

    added rotation and rocket sound effect

--- 3_Project_Boost/Assets/Rocket.cs
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -1,36 +1,59 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Rocket : MonoBehaviour {
-
+    #region Variables
     Rigidbody rigidBody;
+    AudioSource audioSource;
+    #endregion Variables
 
-	// Use this for initialization
-	void Start () {
+    #region Messages
+    // Use this for initialization
+    void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         ProcessInput();
 	}
+    #endregion Messages
 
+    #region Methods
+    //handles inputs
     private void ProcessInput()
+    {
+        ApplyThrust();
+        RotateShip();
+    }
+    //apply relatice force if space is held
+    private void ApplyThrust()
     {
         if (Input.GetKey(KeyCode.Space)) //can trhust while rotating
         {
             rigidBody.AddRelativeForce(Vector3.up);
-        }
 
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Pause();
+        }
+    }
+    //rotates the ship left or right if a or d is pressed
+    private void RotateShip()
+    {
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotating left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating right");
+            transform.Rotate(-Vector3.forward);
         }
     }
+    #endregion Methods
 }
